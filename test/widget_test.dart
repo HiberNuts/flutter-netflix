f23e732581eaf4027ec7bd736fca68f34049dd13
raghav <raghavjindal0212@gmail.com>
2022-04-30 15:14:43 +0530

    edited test files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,14 +17,14 @@ void main() {
 
     // Verify that our counter starts at 0.
     expect(find.text('10'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('10'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('10'), findsNothing);
+    expect(find.text('10'), findsOneWidget);
   });
 }
